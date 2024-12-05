4524bdd1249d4a8d8c203103b733c35762826e9d
Your Name <a>
2024-12-06 01:21:24 +0200

    configure deploy script

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+echo "***************************"
+echo "Deploying the application"
+echo "***************************"
 
+ssh -i $DOCKER_MACHINE_PVK ahmed@192.168.1.6 
+
+docker login -u ahmedelenany703 -p $PASS 
+
+echo "********************"
+echo "Pulling the image"
+echo "********************"
+docker pull ahmedelenany703/maven-image:$BUILD_TAG 
+docker run ahmedelenany703/maven-image:$BUILD_TAG -d 
